799b9ecfee49d346d09455a62454f32b4d85aae0
Henrik Hansen <henrik@webground.dk>
2023-12-07 02:32:31 +0100

    edit stepper-motor file

--- Arduino_V2/stepper_motor_fallerCar/stepper_motor_fallerCar.ino
+++ Arduino_V2/stepper_motor_fallerCar/stepper_motor_fallerCar.ino
@@ -20,30 +20,28 @@
 #include <AccelStepper.h>
 
 // Optical Sensor pin definition:
-const int optic_Sensor_1 2 //optical Sensor on Pin 2 - start position
-const int optic_Sensor_2 3 //optical Sensor on Pin 3 - Car count
-
-// Start stepper motor PushButtom Pin defination:
-const int pushB_start_Pin 4    // Pushbutton to start stepper motor (FOR TEST ONLY)
+#define optic_Sensor_1 2 //optical Sensor on Pin 2 - start position
+#define optic_Sensor_2 3 //optical Sensor on Pin 3 - Car count
+#define stop_start_car 4 //Faccer-Car start/stop signal Pin
 
 // LED for visual indication of movement:
-const int LED_right 5  // LED indicator for moving stepper right
-const int LED_center 6 // LED indicator for moving stepper to center
-const int LED_left 7   // LED indicator for moving stepper left
+#define LED_right 5  // LED indicator for moving stepper right
+#define LED_center 6 // LED indicator for moving stepper to center
+#define LED_left 7   // LED indicator for moving stepper left
 
 // Stepper Motor pin definitions:
-const int motorPin1  8      // IN1 on the ULN2003 driver
-const int motorPin2  9      // IN2 on the ULN2003 driver
-const int motorPin3  10     // IN3 on the ULN2003 driver
-const int motorPin4  11     // IN4 on the ULN2003 driver
+#define motorPin1  8      // IN1 on the ULN2003 driver
+#define motorPin2  9      // IN2 on the ULN2003 driver
+#define motorPin3  10     // IN3 on the ULN2003 driver
+#define motorPin4  11     // IN4 on the ULN2003 driver
 
 // initialize variables
-int optic_Sensor_status = 0; // status of optical sensor
-int step_pos_status = 0; // status of stepper motor position
+int Car_count = 0; // Count variable for optic_Sensor_2
+int stepper_pos = 0; // status of stepper motor position
 int pushb_F1 = HIGH; // status variable for stepper Motor pushbutton
 
 int stepM_speed_right = 500; // speed for moving clockwise (right)
-int stepM_steps_right = 4096; // counts of steps clockwise (right)
+// int stepM_steps_right = 4096; // counts of steps clockwise (right) NOT IN USE
 
 int stepM_speed_left = -500; // Speed for moving counter-clockwise (left)
 int stepM_steps_left = -4096; // counts of steps counter-clockwise (left)
@@ -58,13 +56,11 @@ AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, mo
 // FUNCTION to move right
 void move_right(int stepM_speed_right){
   digitalWrite(LED_right, HIGH); // set LED ON
-
-  optic_Sensor_status = optic_Sensor; // read optical sensor into sensor status variable
   
   // Set the current position to 0:
   stepper.setCurrentPosition(0);
   // Run the motor right at x steps/second until optic sensor == LOW
-  while (digitalRead(optic_Sensor) == HIGH){ // run stepper motor until optical sensor goes LOW
+  while (digitalRead(optic_Sensor_1) == HIGH){ // run stepper motor until optical sensor goes LOW
     stepper.setSpeed(stepM_speed_right); // stepper motor speed
     stepper.runSpeed(); // start stepper motor
   }
@@ -76,61 +72,50 @@ void move_right(int stepM_speed_right){
 
 } // FUNCTION RIGHT END --------------------------------------------
 
-// FUNCTION to move center
-void move_left(int stepM_speed_left, int stepM_steps_left){
-  digitalWrite(LED_IN, HIGH); // set LED IN ON
-
-  sensor_IN = optic_Sens_IN; // read sensor pin to variable sensor_IN
-
-  // Reset the position to 0:
-  stepper.setCurrentPosition(0);
-  // Run the motor backwards at x steps/second until the motor reaches -y steps (z revolution) DOWN/CLOSE : 
-  while (digitalRead(optic_Sens_IN) == HIGH){ // run stepper motor until optical sensor goes LOW
-    stepper.setSpeed(speed_IN);
-    stepper.runSpeed();
-  }
-
-  stepper.stop(); // stop stepper motor
-
-  digitalWrite(LED_IN, LOW); // set LED IN OFF
-
-} //function END -----------------------------------------------
-
-
 // FUNCTION to move left
 void move_left(int stepM_speed_left, int stepM_steps_left){
-  digitalWrite(LED_IN, HIGH); // set LED IN ON
-
-  sensor_IN = optic_Sens_IN; // read sensor pin to variable sensor_IN
-
+  digitalWrite(LED_center, HIGH); // set LED IN ON
+  
   // Reset the position to 0:
-  stepper.setCurrentPosition(0);
-  // Run the motor backwards at x steps/second until the motor reaches -y steps (z revolution) DOWN/CLOSE : 
-  while (digitalRead(optic_Sens_IN) == HIGH){ // run stepper motor until optical sensor goes LOW
-    stepper.setSpeed(speed_IN);
+  stepper.setCurrentPosition(0); // set stepper start position 
+  // Run the motor counter clockwise at x steps/second until the motor reaches -y steps (z revolution) DOWN/CLOSE : 
+  while (stepper.currentPosition() != -4096  ){ // run stepper motor until optical sensor goes LOW
+    stepper.setSpeed(stepM_speed_left);
     stepper.runSpeed();
   }
 
   stepper.stop(); // stop stepper motor
 
-  digitalWrite(LED_IN, LOW); // set LED IN OFF
+  digitalWrite(LED_center, LOW); // set LED IN OFF
 
 } //function END -----------------------------------------------
+
 
 void setup() {
   // put your setup code here, to run once:
 
-  LED_right = LOW;  // LED OFF
-  LED_center = LOW; // LED OFF
-  LED_left = LOW;   // LED ON
+  Serial.begin(9600); // setup and start serial communication default Tx0 / Rx0
 
+  pinMode(optic_Sensor_1, INPUT);
+  pinMode(optic_Sensor_2, INPUT);
+  pinMode(stop_start_car, OUTPUT);
+  pinMode(LED_right, OUTPUT);
+  pinMode(LED_center, OUTPUT);
+  pinMode(LED_left, OUTPUT);
+
+  stepper.setMaxSpeed(500); // max speed for stepper motor
+
+  digitalWrite(LED_right, LOW); // set LED IN OFF
+  digitalWrite(LED_center, LOW); // set LED IN OFF
+  digitalWrite(LED_left, LOW); // set LED IN OFF
+  
   // MOVE STEPPER RIGHT UNTIL OPTICAL SENSOR == HIGH
-
-
+  move_right(stepM_speed_right); // call move right function
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
 
 }
