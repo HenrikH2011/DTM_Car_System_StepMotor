93f58720a361a8e2b78c5011a411010ca3f27b3b
Henrik Hansen <henrik@webground.dk>
2025-04-05 18:40:52 +0200

    edit file - main.cpp
    adjusted and test stepM steps

--- Platformio/DTM_Car_System_01/src/main.cpp
+++ Platformio/DTM_Car_System_01/src/main.cpp
@@ -61,12 +61,12 @@ int speed_ClockW = 500;   // speed to move stepM clockwise
 int speed_CClockW = -500; // speed to move stepM counter-clockwise
 
 // NOTE: this stepM move CCW to IR sensor position
-long stepM_1_StopPos = 200;    // move stepM CW to Stop position
-long stepM_1_IRsensPos = -200; // move stepM CCW to IR sensor position
+long stepM_1_StopPos = 72;    // move stepM CW to Stop position
+long stepM_1_IRsensPos = -80; // move stepM CCW to IR sensor position
 
 // NOTE: this stepM move CCW to IR sensor position
-long stepM_2_StopPos = 72;    // move stepM CW to Stop position
-long stepM_2_IRsensPos = -80; // move stepM CCW to IR sensor position
+long stepM_2_StopPos = 245;    // move stepM CW to Stop position
+long stepM_2_IRsensPos = -250; // move stepM CCW to IR sensor position
 
 // NOTE: this stepM move CW to IR sensor position
 //long stepM_3_StopPos = -200;  // move stepM CCW to Stop position
@@ -265,8 +265,8 @@ void setup() { /****************************************************************
   moveToIRsens(stepM_1, stepM_1_IRsensPos, IR_Sens_1, 1);
   delay(delay_1);
   
-  moveToIRsens(stepM_2, stepM_2_IRsensPos, IR_Sens_2, 2);
-  delay(delay_1);
+ // moveToIRsens(stepM_2, stepM_2_IRsensPos, IR_Sens_2, 2);
+ // delay(delay_1);
   
   //moveToIRsens(stepM_3, stepM_1_IRsensPos, IR_Sens_3, 3);
   //delay(delay_1);
